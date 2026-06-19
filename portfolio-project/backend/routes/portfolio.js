c8fe7dfef91b018962f7f95fec3bee4eac5871ab
PRATHIP M <prathip.muniraju@gmail.com>
2026-06-19 13:37:42 +0530

    Fix: merged code to root directories and removed nested portfolio-project folder

--- portfolio-project/backend/routes/portfolio.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { protect } = require('../middleware/auth');
-const resolveUser = require('../middleware/resolveUser');
-const { getMyLink, getQRCode } = require('../controllers/portfolioController');
-
-router.get('/me/link', protect, getMyLink);
-router.get('/:username/qrcode', resolveUser, getQRCode);
-
-module.exports = router;
